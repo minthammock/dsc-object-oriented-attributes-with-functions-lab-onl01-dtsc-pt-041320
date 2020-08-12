15cccdbe3eeeab618819523eca17ac45ed7aae5d
Michael <michaeljosephmahoney@gmail.com>
2020-08-12 07:42:42 -0600

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,16 @@
 class School:
+  def __init__(self, name, roster = {}):
+    self.name = name
+    self.roster = roster
+    
+  def add_student(self, fullName, gradeLevel):
+    if len(self.roster.get(str(gradeLevel),[])) == 0:
+      self.roster[str(gradeLevel)] = [fullName]
+    else:
+      self.roster[str(gradeLevel)].append(fullName)
+  def grade(self, gradeLevel):
+    return self.roster[str(gradeLevel)]
+  def sort_roster(self):
+    for x in self.roster.keys():
+      self.roster[x].sort()
+    print(self.roster)
